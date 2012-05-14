467e730572577b06b22c79a80ea6e4454f29e97f
jackypan1989 <jackypan1989@gmail.com>
2012-05-14 11:39:29 +0800

    add new parameter

--- src/ntu/im/bilab/panda/jacky/Test.java
+++ src/ntu/im/bilab/panda/jacky/Test.java
@@ -1,5 +1,18 @@
 package ntu.im.bilab.panda.jacky;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -7,13 +20,20 @@ public class Test {
 		//String s=" Inventors:  Price; Edgar E. (Webster, NY) Appl. No.:   05/515,244 Filed:  October 16, 1974";  
 		//String s1=" Inventors:  Carlson; Arthur W. (Muskegon, MI)  Assignee:  E. H. Sheldon and Company (Muskegon, MI)   Appl. No.:   05/535,172 Filed:  December 23, 1974  ";
 		String str=" Inventors:  Schiefer; Harry M. (Midland, MI), Laux; Raymund W. (Munich-Karlsfeld, DT), Grosse; Dietmar W. (Munich, DT)  Assignee:  Dow Corning Corporation (Midland, MI)   Appl. No.:   05/562,292 Filed:  March 26, 1975  ";
-				
+		
+	
+		
 		Test t = new Test();
+		t.getPatentFamilySize("TEST");
+		/*
 		t.getInventors(str);	
 		t.getForeignInventors(str);	
+		t.getForeignClasses(str);	
+		*/
 	}
     
 	/* 	
+	 *  Variable Number : 1
 	 *	Method : return the amount of inventors
 	 * 	Return Type : Integer 
 	 *  Author : Guan-Yu Pan
@@ -42,6 +62,7 @@ public class Test {
 	}		
 	
 	/* 	
+	 *  Variable Number : 2
 	 *	Method : return the amount of foreign inventors by U.S. state code
 	 * 	Return Type : Integer 
 	 *  Author : Guan-Yu Pan
@@ -94,4 +115,113 @@ public class Test {
 		System.out.println("foreign_inventors : "+foreign_inventors);
 		return foreign_inventors;
 	}
+	
+	/* 	
+	 *  Variable Number : 6
+	 *	Method : return the amount of foreign cited patent by uspto foreign document field(?) 
+	 * 	Return Type : Integer 
+	 *  Author : Guan-Yu Pan
+	 * 	Last Edit Date : 20120425
+	 *  Example : "U.S. Patent Documents     D191499 October 1961 Donay D233586 November 1974 Kopp D261601 November 1981 Kettlestrings D287093 December 1986 Ryan 1388282 August 1921 Meredith   Foreign Patent Documents     4615 ., 1891 GB     Primary Examiner:  Burke; Wallace R. <BR> Assistant Examiner:  Tabor; Lavone D. <BR> Attorney, Agent or Firm: Zarley, McKee, Thomte, Voorhees & Sease <BR>";
+	 */ 
+	public int getForeignClasses(String patent_id){
+		
+		String data = "U.S. Patent Documents     D191499 October 1961 Donay D233586 November 1974 Kopp D261601 November 1981 Kettlestrings D287093 December 1986 Ryan 1388282 August 1921 Meredith   Foreign Patent Documents     4615 ., 1891 GB     Primary Examiner:  Burke; Wallace R. <BR> Assistant Examiner:  Tabor; Lavone D. <BR> Attorney, Agent or Firm: Zarley, McKee, Thomte, Voorhees & Sease <BR>";
+	    int foreign_classes = 0;
+		
+	    // clarify data, some with other references 
+	    if(data.contains("Foreign Patent Documents")){
+			if(data.contains("Other References")){
+				data = data.substring(data.indexOf("Foreign Patent Documents")+25,data.indexOf("Other References"));	
+			}else{
+				data = data.substring(data.indexOf("Foreign Patent Documents")+25,data.indexOf("Primary Examiner"));
+			}
+			
+			// calculate if there is foreign patent
+			String[] foreign_patents = data.split(",");
+			foreign_classes = foreign_patents.length-1;
+			
+		}
+		System.out.println("foreign_classes : "+foreign_classes);
+		return foreign_classes;
+	    
+	    /* direct fetch from uspto web 
+	    StringBuilder document= new StringBuilder();
+		try {
+			URL url = new URL("http://patft1.uspto.gov/netacgi/nph-Parser?patentnumber="+patent_id);
+			URLConnection conn = url.openConnection();
+			conn.setConnectTimeout(30000);
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		    String line = null;
+		    while ((line = reader.readLine()) != null)
+		    	document.append(line + "\n");
+		    reader.close();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/  
+	}
+	
+	/* 	
+	 *  Variable Number : 6
+	 *	Method : return the size of patent family from EPO  
+	 * 	Return Type : Integer 
+	 *  Author : Guan-Yu Pan
+	 * 	Last Edit Date : 20120430
+	 *  Example : "http://worldwide.espacenet.com/publicationDetails/inpadocPatentFamily?CC=US&NR=RE37489E1&FT=D";
+	 */ 
+	public int getPatentFamilySize(String patent_id){
+	    int patent_family_size = 0;
+		
+	    // fetch from EPO
+		try {
+			Document doc = Jsoup.connect("http://worldwide.espacenet.com/publicationDetails/inpadocPatentFamily?CC=US&NR=RE37489E1&FT=D").get();
+			Element s = doc.getElementsByClass("epoBarItem").first().getElementsByTag("strong").first();
+			patent_family_size = Integer.parseInt(s.text());
+			System.out.println(s.text());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return patent_family_size;
+	}
+	
+    /*
+	public int getPatentFamilySize(String patent_id){
+		int patent_family_size = 0;
+		
+	    StringBuilder document= new StringBuilder();
+		try {
+			URL url = new URL("http://worldwide.espacenet.com/publicationDetails/inpadocPatentFamily?CC=US&FT=D&NR="+patent_id);
+			URLConnection conn = url.openConnection();
+			conn.setConnectTimeout(30000);
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		    String line = null;
+		    while ((line = reader.readLine()) != null)
+		    	document.append(line + "\n");
+		    reader.close();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String data = document.toString();
+		
+		data = data.substring(data.indexOf("epoBarItem"),data.indexOf("application"));
+		Pattern p = Pattern.compile("+[0-9]");
+		String patent_family = Pattern.quote(data);
+		patent_family_size = Integer.valueOf(patent_family);
+		
+		
+		return patent_family_size;
+	}
+	*/
 }
